8ffdf7d5dcf99cc5656c242fc8a55c7ce5a0fd37
Viacheslav Leonov <viacheslav.leonov@idealo.de>
2022-04-11 14:46:34 +0200

    connected message deleted

--- handlers.py
+++ handlers.py
@@ -9,13 +9,6 @@ def start(update, context):
 
     user_info = update.message.from_user.to_dict()
 
-    context.bot.send_message(
-        chat_id=TELEGRAM_SUPPORT_CHAT_ID,
-        text=f"""
-📞 Connected {user_info}.
-        """,
-    )
-
 
 def forward_to_chat(update, context):
     """{ 
